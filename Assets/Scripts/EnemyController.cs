8e93dc4042664a7eb5946e45132811ae29e7389b
Clirfy <48675064+Clirfy@users.noreply.github.com>
2021-06-09 17:31:53 +0200

    added: enemy destroys when reaches tower

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,10 +6,18 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    private bool IsOnDestinationGround{get; set;}
+
     public void Initialize (Vector3 spawnPosition, Vector3 endPosition)
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.Warp(spawnPosition);
         agent.destination = endPosition;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("hit)");
+        Destroy(gameObject);
     }
 }
