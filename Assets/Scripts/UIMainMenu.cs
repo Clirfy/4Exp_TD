a1803677851c4b3ac387629dddf22b720ed0b154
Clirfy <48675064+Clirfy@users.noreply.github.com>
2021-06-16 19:16:51 +0200

    added UI Canvas

---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public void GoToLevelSelection()
+    {
+        
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
